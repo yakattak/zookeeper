9ae4e645a17182b3a31ee66738bd8e5fb7ca4563
yakattak <stevecalebrese@gmail.com>
2022-03-03 19:29:45 -0500

    ;lkj

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.urlencoded({ extended: true}));
 //parse incoming json data
 app.use(express.json());
 
+app.use(express.static('public'));
+
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -115,6 +118,22 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
